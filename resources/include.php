d3d997a4f96610c5435bcc91331bb75aff411f35
Guido Terceros Fernandez <guido.terceros@gmail.com>
2015-09-29 13:32:43 -0400

    Libs

---
+++ resources/include.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * @Autor: Guido Terceros Fernandez
+ * @Email: guido.terceros@gmail.com
+ */
+
+require_once 'classes/tools/sis_tools_server.php';
+require_once 'lib/phpword/PHPWord.php';
+require_once 'lib/FirePHP.php';
+
+?>
